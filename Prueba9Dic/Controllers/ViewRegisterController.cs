3b935154280799db0ae3d3546c3c53ade62ad309
pedrolandaacurio <pedrolandaacurio@hotmail.com>
2017-12-11 17:46:36 -0500

    Inicio

---
+++ Prueba9Dic/Controllers/ViewRegisterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prueba9Dic.Controllers
+{
+    public class ViewRegisterController : Controller
+    {
+        // GET: ViewRegister
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Prueba9Dic/ViewModel/ViewModel1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba9Dic.ViewModel
+{
+    public class ViewModel1
+    {
+        public int Id_Sitio { get; set; }
+        public string CodigoSitio { get; set; }
+        public string Coord_X { get; set; }
+        public string Coord_Y { get; set; }
+        public Nullable<decimal> Altitud { get; set; }
+        public Nullable<int> Propiedad { get; set; }
+        public string Antecedentes { get; set; }
+        public string RelatoAcontecimientos { get; set; }
+        public Nullable<System.DateTime> FechaEvento { get; set; }
+        public Nullable<System.DateTime> FechaInhumacion { get; set; }
+        public Nullable<decimal> NumeroVictimas { get; set; }
+        public Nullable<bool> ExhumacionAnterior { get; set; }
+        public Nullable<bool> MaterialesAdicionales { get; set; }
+        public List<Informantes> ListInformantes { get; set; }
+        public List<Victimas> ListVictimas { get; set; }
+        public List<Familiares> ListFamiliares { get; set; }
+        public List<Testigos> ListTestigos { get; set; }
+    }
+}
